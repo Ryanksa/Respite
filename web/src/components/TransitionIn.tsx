c7988c2c781d863e04b92173ccdd3911b0b1ace4
Ryanksa <ryanlin58@gmail.com>
2023-02-26 12:28:13 -0500

    Moved assets to public

--- web/src/components/TransitionIn.tsx
+++ web/src/components/TransitionIn.tsx
@@ -1,19 +1,4 @@
 import styles from "~/styles/TransitionIn.module.css";
-import bread from "~/assets/bread.jpg";
-import bruchetta from "~/assets/bruchetta.jpg";
-import cheeseBoard from "~/assets/cheese-board.jpg";
-import curry from "~/assets/curry.jpg";
-import hatYai from "~/assets/hat-yai.jpg";
-import kimchiJjigae from "~/assets/kimchi-jjigae.jpg";
-import macaron from "~/assets/macaron.jpg";
-import malvaPudding from "~/assets/malva-pudding.jpg";
-import milkTea from "~/assets/milk-tea.jpg";
-import pho from "~/assets/pho.jpg";
-import shavedIce from "~/assets/shaved-ice.jpg";
-import steak from "~/assets/steak.jpg";
-import tsukemen from "~/assets/tsukemen.jpg";
-import wuGeng from "~/assets/wu-geng.jpg";
-import restaurant from "~/assets/restaurant-bg.jpg";
 
 export default function TransitionIn() {
   return (
@@ -25,84 +10,112 @@ export default function TransitionIn() {
         <div
           class={`${styles.box} ${styles.box1} ${styles.row1} flex rounded-lg overflow-hidden`}
         >
-          <img src={cheeseBoard} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/cheese-board.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
         <div
           class={`${styles.box} ${styles.box2} ${styles.row1} flex rounded-lg overflow-hidden`}
         >
-          <img src={bread} class="object-cover m-auto rounded-lg" />
+          <img src="/images/bread.jpg" class="object-cover m-auto rounded-lg" />
         </div>
         <div
           class={`${styles.box} ${styles.box3} ${styles.row1} flex rounded-lg overflow-hidden`}
         >
-          <img src={bruchetta} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/bruchetta.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
         <div
           class={`${styles.box} ${styles.box4} ${styles.row1} flex rounded-lg overflow-hidden`}
         >
-          <img src={steak} class="object-cover m-auto rounded-lg" />
+          <img src="/images/steak.jpg" class="object-cover m-auto rounded-lg" />
         </div>
         <div
           class={`${styles.box} ${styles.box5} ${styles.row1} flex rounded-lg overflow-hidden`}
         >
-          <img src={malvaPudding} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/malva-pudding.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
 
         <div
           class={`${styles.box} ${styles.row2} ${styles.box6} flex rounded-lg overflow-hidden`}
         >
-          <img src={kimchiJjigae} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/kimchi-jjigae.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
         <div
           class={`${styles.box} ${styles.row2} ${styles.box7} flex rounded-lg overflow-hidden`}
         >
-          <img src={hatYai} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/hat-yai.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
         <div
           class={`${styles.box} ${styles.row2} ${styles.box8} flex rounded-lg overflow-hidden`}
         >
-          <img src={restaurant} class="object-cover m-auto rounded-lg" />
-          <h1
-            class={`${styles.fadeOut} absolute top-1/2 -translate-y-1/2 w-full text-[0.5rem] logo z-10`}
-          >
-            Respite
-          </h1>
+          <img
+            src="/images/restaurant.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
         <div
           class={`${styles.box} ${styles.row2} ${styles.box9} flex rounded-lg overflow-hidden`}
         >
-          <img src={milkTea} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/milk-tea.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
         <div
           class={`${styles.box} ${styles.row2} ${styles.box10} flex rounded-lg overflow-hidden`}
         >
-          <img src={shavedIce} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/shaved-ice.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
 
         <div
           class={`${styles.box} ${styles.row3} ${styles.box1} flex rounded-lg overflow-hidden`}
         >
-          <img src={pho} class="object-cover m-auto rounded-lg" />
+          <img src="/images/pho.jpg" class="object-cover m-auto rounded-lg" />
         </div>
         <div
           class={`${styles.box} ${styles.row3} ${styles.box2} flex rounded-lg overflow-hidden`}
         >
-          <img src={tsukemen} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/tsukemen.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
         <div
           class={`${styles.box} ${styles.row3} ${styles.box3} flex rounded-lg overflow-hidden`}
         >
-          <img src={wuGeng} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/wu-geng.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
         <div
           class={`${styles.box} ${styles.row3} ${styles.box4} flex rounded-lg overflow-hidden`}
         >
-          <img src={curry} class="object-cover m-auto rounded-lg" />
+          <img src="/images/curry.jpg" class="object-cover m-auto rounded-lg" />
         </div>
         <div
           class={`${styles.box} ${styles.row3} ${styles.box5} flex rounded-lg overflow-hidden`}
         >
-          <img src={macaron} class="object-cover m-auto rounded-lg" />
+          <img
+            src="/images/macaron.jpg"
+            class="object-cover m-auto rounded-lg"
+          />
         </div>
       </div>
     </div>
